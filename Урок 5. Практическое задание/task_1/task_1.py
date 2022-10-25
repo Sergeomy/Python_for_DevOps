1102584a199b86351c09e5ca0ca74a7f5f74ecad
Sergey Malyugin <sergeony@mail.ru>
2022-10-25 19:12:23 +0300

    ДЗ к уроку 5.

--- Урок 5. Практическое задание/task_1/task_1.py
+++ Урок 5. Практическое задание/task_1/task_1.py
@@ -3,3 +3,18 @@
 записать в него построчно данные, вводимые пользователем.
 Об окончании ввода данных свидетельствует пустая строка.
 """
+
+fl = open("file_1.txt", "w", encoding="utf-8")
+data = input("Введите данные (пустая строка - выход): ")
+
+while True:
+    fl.writelines(data + "\n")
+    data = input("Введите данные (пустая строка - выход): ")
+    if data == '':
+        break
+
+fl.close()
+fl = open("file_1.txt", "r")
+body = fl.readlines()
+print(body)
+fl.close()
